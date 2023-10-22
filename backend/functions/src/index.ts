d9207edf651f88266832872c9d93d8c9c588828d
mdmusaibali <mdmusaibali@gmail.com>
2023-10-22 17:45:06 +0530

    Replacing random hadith generator in backend

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -30,32 +30,64 @@ exports.sendDailyNotification = pubsub
   .timeZone("Asia/Kolkata")
   .onRun(async (context) => {
     try {
-      // get hadith (Hadith api)
-      const randomHadith = await getRandomHadith();
       // get unsplash url
       const imgUrl = await getUnsplashImageUrl();
+      // For hadith's from hadithapi.com (DEPRICATED FOR NOW)//////////////////////////////////////
       // doing this because fcm supports max payload size of 4kb and then there's imgUrl and title.
-      // empty string check
-      if (randomHadith) {
-        // payload length check
-        if (randomHadith.length > 3500 || randomHadith.length < 85) {
-          // if exceeds 3500 then get new hadith and send
-          const newRandomHadith = await getRandomHadith();
-          if (newRandomHadith) {
-            sendMessage(newRandomHadith, imgUrl);
-          }
-        } else {
-          // else send the current one
-          sendMessage(randomHadith, imgUrl);
-        }
-      } else {
-        // if empty string then get new hadith and send
-        const newRandomHadith = await getRandomHadith();
-        if (newRandomHadith) {
-          sendMessage(newRandomHadith, imgUrl);
-        }
+      // if (randomHadith) {
+      //   if (randomHadith.length > 3500 || randomHadith.length < 85) {
+      //     const newRandomHadith = await getRandomHadith();
+      //     if (newRandomHadith) {
+      //       console.log(newRandomHadith, imgUrl);
+      //       // sendMessage(newRandomHadith, imgUrl);
+      //     }
+      //   } else {
+      //     console.log(randomHadith, imgUrl);
+      //     // sendMessage(randomHadith, imgUrl);
+      //   }
+      // } else {
+      //   const newRandomHadith = await getRandomHadith();
+      //   if (newRandomHadith) {
+      //     console.log(newRandomHadith, imgUrl);
+      //     // sendMessage(newRandomHadith, imgUrl);
+      //   }
+      // }
+      ////////////////////////////////////////////////////////////////////////////////////////////
+      // 3 try mechanism
+      // TRY 1
+      let randomHadith;
+      randomHadith = await getRandomHadith();
+      if (
+        randomHadith &&
+        randomHadith.length < 3500 &&
+        randomHadith.length > 85
+      ) {
+        sendMessage(randomHadith, imgUrl);
+        return null;
       }
-      return null;
+      // TRY 2
+      randomHadith = await getRandomHadith();
+      if (
+        randomHadith &&
+        randomHadith.length < 3500 &&
+        randomHadith.length > 85
+      ) {
+        sendMessage(randomHadith, imgUrl);
+        return null;
+      }
+      // TRY 3
+      randomHadith = await getRandomHadith();
+      if (
+        randomHadith &&
+        randomHadith.length < 3500 &&
+        randomHadith.length > 85
+      ) {
+        sendMessage(randomHadith, imgUrl);
+        return null;
+      }
+      throw new Error(
+        "Hadith did not meet criterias in 3 try. Notification failed."
+      );
     } catch (error) {
       console.log("Something went wrong", error);
       return null;
@@ -67,35 +99,74 @@ exports.sendNotification = onRequest(async (req, res) => {
   try {
     const paramKey = req.query?.key;
     if (!paramKey) {
-      res.send("Please send trigger key");
+      res.send("Please send trigger key as param");
       return;
     }
     if (paramKey !== process.env.TRIGGER_KEY) {
       res.send("Invalid trigger key");
       return;
     }
-    // get hadith (Hadith api)
-    const randomHadith = await getRandomHadith();
     // get unsplash url
     const imgUrl = await getUnsplashImageUrl();
+    // For hadith's from hadithapi.com (DEPRICATED FOR NOW)//////////////////////////////////////
     // doing this because fcm supports max payload size of 4kb and then there's imgUrl and title.
-    if (randomHadith) {
-      if (randomHadith.length > 3500 || randomHadith.length < 85) {
-        const newRandomHadith = await getRandomHadith();
-        if (newRandomHadith) {
-          sendMessage(newRandomHadith, imgUrl);
-        }
-      } else {
-        sendMessage(randomHadith, imgUrl);
-      }
-    } else {
-      const newRandomHadith = await getRandomHadith();
-      if (newRandomHadith) {
-        sendMessage(newRandomHadith, imgUrl);
-      }
+    // if (randomHadith) {
+    //   if (randomHadith.length > 3500 || randomHadith.length < 85) {
+    //     const newRandomHadith = await getRandomHadith();
+    //     if (newRandomHadith) {
+    //       console.log(newRandomHadith, imgUrl);
+    //       // sendMessage(newRandomHadith, imgUrl);
+    //     }
+    //   } else {
+    //     console.log(randomHadith, imgUrl);
+    //     // sendMessage(randomHadith, imgUrl);
+    //   }
+    // } else {
+    //   const newRandomHadith = await getRandomHadith();
+    //   if (newRandomHadith) {
+    //     console.log(newRandomHadith, imgUrl);
+    //     // sendMessage(newRandomHadith, imgUrl);
+    //   }
+    // }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    // 3 try mechanism
+    // TRY 1
+    let randomHadith;
+    randomHadith = await getRandomHadith();
+    if (
+      randomHadith &&
+      randomHadith.length < 3500 &&
+      randomHadith.length > 85
+    ) {
+      sendMessage(randomHadith, imgUrl);
+      res.send("Notification sent");
+      return;
     }
-    res.send("Notification sent");
-    return;
+    // TRY 2
+    randomHadith = await getRandomHadith();
+    if (
+      randomHadith &&
+      randomHadith.length < 3500 &&
+      randomHadith.length > 85
+    ) {
+      sendMessage(randomHadith, imgUrl);
+      res.send("Notification sent");
+      return;
+    }
+    // TRY 3
+    randomHadith = await getRandomHadith();
+    if (
+      randomHadith &&
+      randomHadith.length < 3500 &&
+      randomHadith.length > 85
+    ) {
+      sendMessage(randomHadith, imgUrl);
+      res.send("Notification sent");
+      return;
+    }
+    throw new Error(
+      "Hadith did not meet criterias in 3 try. Notification failed."
+    );
   } catch (error) {
     console.log("Something went wrong", error);
     res.status(500).send(error);
@@ -121,7 +192,7 @@ exports.sendCustomNotification = onRequest(async (req, res) => {
     }
 
     sendCustomMessage(title, body, imageUrl);
-    res.send("Notification sent.")
+    res.send("Notification sent.");
     return;
   } catch (error) {
     if (error instanceof Error) res.status(500).send(error.message);
--- backend/functions/src/utils/helpers.ts
+++ backend/functions/src/utils/helpers.ts
@@ -1,56 +1,84 @@
 import { messaging } from "firebase-admin";
 import axios from "axios";
 
-const bookSlugs: string[] = [
-  "sahih-bukhari",
-  "sahih-muslim",
-  "al-tirmidhi",
-  "abu-dawood",
-  "ibn-e-majah",
-];
-
-const MAX_CHAP = 39;
-
 export function pickRandomItem(array: any[]) {
   const length = array.length;
   const randomIndex = Math.floor(Math.random() * length);
   return array[randomIndex];
 }
 
-function getRandomNumber(upper_bound: number) {
-  const randomNumber = Math.random();
-  const generatedNumber = Math.floor(randomNumber * upper_bound) + 1;
-  return generatedNumber;
-}
+// For hadith's from hadithapi.com (DEPRICATED FOR NOW) //////////////////////////////////////////////////////////////////
+// const bookSlugs: string[] = [
+//   "sahih-bukhari",
+//   "sahih-muslim",
+//   "al-tirmidhi",
+//   "abu-dawood",
+//   "ibn-e-majah",
+// ];
 
-export const randomHadithUrl = () => {
-  const bookSlug = pickRandomItem(bookSlugs);
-  const chapterNumber = getRandomNumber(MAX_CHAP);
-  const url = `https://hadithapi.com/api/hadiths/?apiKey=${process.env.HADITH_API_KEY}&book=${bookSlug}&chapter=${chapterNumber}`;
-  return url;
-};
+// const MAX_CHAP = 39;
 
+// function getRandomNumber(upper_bound: number) {
+//   const randomNumber = Math.random();
+//   const generatedNumber = Math.floor(randomNumber * upper_bound) + 1;
+//   return generatedNumber;
+// }
+
+// export const randomHadithUrl = () => {
+//   const bookSlug = pickRandomItem(bookSlugs);
+//   const chapterNumber = getRandomNumber(MAX_CHAP);
+//   const url = `https://hadithapi.com/api/hadiths/?apiKey=${process.env.HADITH_API_KEY}&book=${bookSlug}&chapter=${chapterNumber}`;
+//   return url;
+// };
+
+// export const getRandomHadith = async () => {
+//   const randomUrl = randomHadithUrl();
+//   const apiResponse = await axios.get(randomUrl);
+//   const responseData = apiResponse.data;
+//   const hadithData = responseData?.hadiths?.data;
+//   if (hadithData && Array.isArray(hadithData)) {
+//     const randomHadithObj = pickRandomItem(hadithData);
+//     const englishNarrator = randomHadithObj?.englishNarrator;
+//     const hadithEnglish = randomHadithObj?.hadithEnglish;
+//     const volume = randomHadithObj?.volume;
+//     const hadithNumber = randomHadithObj?.hadithNumber;
+//     const bookName = randomHadithObj?.book?.bookName;
+//     let randomHadithString: string;
+//     if (englishNarrator) {
+//       randomHadithString = englishNarrator + " " + hadithEnglish;
+//     } else {
+//       randomHadithString = hadithEnglish;
+//     }
+//     return (
+//       randomHadithString.trim() +
+//       `\n\n(${bookName || ""} vol.${volume || ""} #${hadithNumber || ""})`
+//     );
+//   }
+//   return null;
+// };
+////////////////////////////////////////////////////////////////////////////////////
+const bookSlugs: string[] = [
+  "bukhari",
+  "muslim",
+  "tirmidhi",
+  "abudawud",
+  "ibnmajah",
+];
 export const getRandomHadith = async () => {
-  const randomUrl = randomHadithUrl();
-  const apiResponse = await axios.get(randomUrl);
-  const responseData = apiResponse.data;
-  const hadithData = responseData?.hadiths?.data;
-  if (hadithData && Array.isArray(hadithData)) {
-    const randomHadithObj = pickRandomItem(hadithData);
-    const englishNarrator = randomHadithObj?.englishNarrator;
-    const hadithEnglish = randomHadithObj?.hadithEnglish;
-    const volume = randomHadithObj?.volume;
-    const hadithNumber = randomHadithObj?.hadithNumber;
-    const bookName = randomHadithObj?.book?.bookName;
-    let randomHadithString: string;
-    if (englishNarrator) {
-      randomHadithString = englishNarrator + " " + hadithEnglish;
-    } else {
-      randomHadithString = hadithEnglish;
-    }
+  const bookSlug = pickRandomItem(bookSlugs);
+  const response = await axios.get(
+    `https://random-hadith-generator.vercel.app/${bookSlug}/`
+  );
+  const data = response.data;
+  const randomHadithObj = data?.data;
+  const randomHadithString: string | null = randomHadithObj?.hadith_english;
+  const randomHadithChapter = randomHadithObj?.chapterName;
+  const randomHadithRef = randomHadithObj?.refno;
+  if (randomHadithString) {
     return (
-      randomHadithString.trim() +
-      `\n\n(${bookName || ""} vol.${volume || ""} #${hadithNumber || ""})`
+      `${randomHadithChapter.trim() || ""}` +
+      `\n\n${randomHadithString.trim() || ""}` +
+      `\n\n ${randomHadithRef || ""}`
     );
   }
   return null;
